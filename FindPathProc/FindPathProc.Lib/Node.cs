be1b8b44ba735c2a6ce18cbf08db7a8aef7c8d4a
Valerii_kor <myksv200@gmail.com>
2021-05-21 23:03:15 +0400

    Priority Queue
    Created PriorityQueue

---
+++ FindPathProc/FindPathProc.Lib/Node.cs
@@ -0,0 +1,66 @@
+﻿namespace FindPathProc.Lib
+{
+    public class Node<T>
+    {
+        public Node<T> LChild => _lChild;
+        public Node<T> RChild => _rChild;
+        private Node<T> _mother;
+        private Node<T> _lChild;
+        private Node<T> _rChild;
+        private readonly double _priority;
+        
+        public T Value { get; }
+
+        public void DeleteNode()
+        {
+            if (_rChild!=null)
+            {
+                _mother._lChild = _rChild;
+                _mother._lChild._mother = _mother;
+            }
+            else
+            {
+                _mother._lChild = null;
+            }
+            
+        }
+        public Node(Node<T> mother, T value, double priority)
+        {
+            _mother = mother;
+            _priority = priority;
+            Value = value;
+        }
+
+        
+
+        
+        
+        public static void AddElement(Node<T> node, T element, double priority)
+        {
+            if (priority<= node._priority)
+            {
+                if (node._lChild==null)
+                {
+                    node._lChild = new Node<T>(node, element, priority);
+                }
+                else
+                {
+                    AddElement(node._lChild, element, priority);
+                }
+
+            }else if (priority > node._priority)
+            {
+                if (node._rChild==null)
+                {
+                    node._rChild = new Node<T>(node, element, priority);
+                }
+                else
+                {
+                    AddElement(node._rChild, element, priority);
+                }
+            }
+        }
+        
+        
+    }
+}
---
+++ FindPathProc/FindPathProc.Lib/PriorityQueue.cs
@@ -0,0 +1,70 @@
+﻿namespace FindPathProc.Lib
+{
+    public class PriorityQueue<T>
+    {
+        private Node<T> _root;
+
+        public PriorityQueue()
+        {
+        }
+
+        public PriorityQueue(T element, double priority)
+        {
+            _root = new Node<T>(null, element, priority);
+        }
+
+        public void AddElement(T element, double priority)
+        {
+            if (_root == null)
+            {
+                _root = new Node<T>(null, element, priority);
+            }
+            else
+            {
+                Node<T>.AddElement(_root, element, priority);
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            if (_root == null)
+                return true;
+            return false;
+        }
+
+
+        private Node<T> Dequeue(Node<T> currentNode)
+        {
+            if (currentNode.LChild != null)
+            {
+                return Dequeue(currentNode.LChild);
+            }
+
+            return currentNode;
+        }
+
+        public T Dequeue()
+        {
+            Node<T> targetNode = Dequeue(_root);
+            T value = targetNode.Value;
+            if (targetNode == _root)
+            {
+                if (_root.RChild != null)
+                {
+                    _root = _root.RChild;
+                }
+                else
+                {
+                    _root = null;
+                }
+                
+            }
+            else
+            {
+                targetNode.DeleteNode();
+            }
+
+            return value;
+        }
+    }
+}
